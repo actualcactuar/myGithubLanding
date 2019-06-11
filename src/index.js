342b931a1d2b94aad6f6e787282cd145e0bfb6bf
actualcactuar <kiviniemi.jiri@gmail.com>
2019-06-11 09:47:54 +0300

    Update text and add share API

--- src/index.js
+++ src/index.js
@@ -20,3 +20,32 @@ let form = document.querySelector('.dynamic-form');
 if (form) {
     new DynamicForm(form);
 }
+
+
+// Sharing the page
+let sharer = document.querySelector('#share');
+if(sharer){
+
+    let shareCase = navigator.share ? 'sheet' : 'prompt';
+
+    let shareOptions = {
+        'sheet':() => {
+            navigator.share({
+                title: 'Actualcactuar',
+                text: 'Homepage / showcase of my favourite technologies',
+                url: window.location.href
+              }).catch(err => {
+                prompt("Erroc occured while sharing, here is a snippet!", window.location.href);
+                  
+              })
+        },
+        'prompt': () => {
+            prompt("Your browser doesn't support share API, but here is a snippet!", window.location.href);
+        }
+    }
+
+    sharer.addEventListener('click', () => {
+        shareOptions[shareCase].call();
+    });
+
+}
