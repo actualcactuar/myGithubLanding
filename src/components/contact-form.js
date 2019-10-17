c0bcc9a4145b92ca6a3f93f07b827797b422a222
jiri <kiviniemi.jiri@gmail.com>
2019-10-17 14:05:04 +0300

    lint

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,44 +1,40 @@
 import * as axios from 'axios';
 
 export class DynamicForm {
-    
-    constructor(element){
-        this.host = element;
-        this.inputs = this.host.querySelectorAll('input,textarea,select');
-        this.host.addEventListener('submit', event => {
-            this.send(event);
-        });
-        this.submitButton = this.host.querySelector('button[type="submit"]');
-    }
+  constructor(element) {
+    this.host = element;
+    this.inputs = this.host.querySelectorAll('input,textarea,select');
+    this.host.addEventListener('submit', event => {
+      this.send(event);
+    });
+    this.submitButton = this.host.querySelector('button[type="submit"]');
+  }
 
-    get data () {
-        let data = {};
-        this.inputs.forEach(input => data[input.getAttribute('name')] = input.value);
-        return data;
-    }
+  get data() {
+    let data = {};
+    this.inputs.forEach(input => {
+      const key = input.getAttribute('name');
+      const { value } = input;
+      data = { ...data, [key]: value };
+    });
+    return data;
+  }
 
-    send (event) {
+  async send(event) {
+    this.submitButton.classList.add('active');
 
-        this.submitButton.classList.add('active');
-        
-        event.preventDefault();
+    event.preventDefault();
+    await axios.post(this.host.action, this.data, {
+      headers: {
+        'Content-Type': this.host.getAttribute('enctype'),
+      },
+    });
+    this.host.reset();
+    this.submitButton.classList.remove('active');
+    this.submitButton.classList.add('completed');
 
-        axios.post(this.host.action,this.data,{
-            headers: {
-                "Content-Type": this.host.getAttribute('enctype'),
-            },
-        })
-        .then(res => res.data)
-        .then(json => {
-            this.host.reset();
-            this.submitButton.classList.remove('active');
-            this.submitButton.classList.add('completed');
-
-            setTimeout(()=>{;
-                this.submitButton.classList.remove('completed');
-            },1000)
-        })
-
-    }
-
+    setTimeout(() => {
+      this.submitButton.classList.remove('completed');
+    }, 1000);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -40,10 +40,12 @@ if (sharer) {
           url: window.location.href,
         })
         .catch(() => {
+          // eslint-disable-next-line no-alert
           prompt('Error occured while sharing, here is a snippet!', window.location.href);
         });
     },
     prompt: () => {
+      // eslint-disable-next-line no-alert
       prompt("Your browser doesn't support the share API, but here is a snippet!", window.location.href);
     },
   };
@@ -53,30 +55,7 @@ if (sharer) {
   });
 }
 
-const engine = new TemplateRendererEngine();
-const newsContainer = document.querySelector('#hNewsContainer');
-const tpl = newsContainer ? newsContainer.innerHTML : null;
-const storyUrl = id => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
-
-const fetchDetails = async id => {
-  const { data } = await axios.get(storyUrl(id));
-  return data;
-};
-
-const fetchTopTenHackerNews = async () => {
-  const { data } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
-  const topTen = data.slice(0, 10);
-  Promise.all(topTen.map(id => fetchDetails(id)))
-    // eslint-disable-next-line no-return-assign
-    .then(values => (newsContainer.innerHTML = engine.render(tpl, { news: values })))
-    .then(() => createObserver('.hacker-news-entry'));
-};
-
-if (tpl && newsContainer) {
-  fetchTopTenHackerNews();
-}
-
-function createObserver(selector) {
+const createObserver = selector => {
   const cols = document.querySelectorAll(selector);
 
   if (!('IntersectionObserver' in window)) {
@@ -94,10 +73,33 @@ function createObserver(selector) {
       observer.observe(col);
     });
   }
+};
+
+const engine = new TemplateRendererEngine();
+const newsContainer = document.querySelector('#hNewsContainer');
+const tpl = newsContainer ? newsContainer.innerHTML : null;
+const storyUrl = id => `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
+
+const fetchDetails = async id => {
+  const { data } = await axios.get(storyUrl(id));
+  return data;
+};
+
+const fetchTopTenHackerNews = async () => {
+  const { data } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+  const topTen = data.slice(0, 10);
+  const values = await Promise.all(topTen.map(id => fetchDetails(id)));
+  newsContainer.innerHTML = engine.render(tpl, { news: values });
+  createObserver('.hacker-news-entry');
+};
+
+if (tpl && newsContainer) {
+  fetchTopTenHackerNews();
 }
 
 createObserver('[class^="col-"]');
-document.addEventListener('DOMContentLoaded', event => {
+
+document.addEventListener('DOMContentLoaded', () => {
   const themeSwitch = document.querySelector('c-theme-switch');
   const cachedTheme = localStorage.getItem('cachedTheme');
   if (cachedTheme && themeSwitch) {
